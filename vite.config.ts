acbf236478272235f87d4530e50df8b4b1b90fb8
argeento <adrian.wieprzkowicz@gmail.com>
2022-06-08 00:33:23 +0200

    Fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/skiercon-overlay/',
   build: {
     outDir: 'docs'
   }
